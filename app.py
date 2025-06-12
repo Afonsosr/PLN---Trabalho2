285545d1ed7ace719f66a803d93bda42cd7edb27
a95074 <92465292+a95074@users.noreply.github.com>
2025-06-12 15:28:57 +0100

    conceitos

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ file = "glossario_por_categoria.json"
 with open(file, 'r', encoding='utf-8') as file:
     conceitos = json.load(file)
 
-def guardar_conceitos(doencas, filename):
+def guardar_conceitos(conceitos, filename):
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(conceitos, file, ensure_ascii=False, indent=4)
         
@@ -24,5 +24,33 @@ def carregar_conceitos(file):
 def home():
     tamanho = len(conceitos)
     return render_template("home.html", tamanho = tamanho)
+
+@app.route("/conceitos")
+def listar_conceitos():
+    conceitos = carregar_conceitos("glossario_por_categoria.json")
+    conceitos_por_letra = defaultdict(list)
+
+    for categoria, lista_conceitos in conceitos.items():
+        for item in lista_conceitos:
+            conceito = item.get("Conceito")
+            if conceito:
+                primeira_letra = conceito[0].upper()
+
+                # Junta as letras com acento à letra normal
+                if primeira_letra in ["Á", "Â"]:
+                    conceitos_por_letra["A"].append(conceito)
+                elif primeira_letra == "É":
+                    conceitos_por_letra["E"].append(conceito)
+                elif primeira_letra == "Í":
+                    conceitos_por_letra["I"].append(conceito)
+                elif primeira_letra == "Ó":
+                    conceitos_por_letra["O"].append(conceito)
+                elif primeira_letra == "Ú":
+                    conceitos_por_letra["U"].append(conceito)
+                else:
+                    conceitos_por_letra[primeira_letra].append(conceito)
+
+    return render_template("conceitos.html", conceitos_por_letra=conceitos_por_letra)
+
 
 app.run(host="localhost", port=4001, debug=True)
